baea0425eb488129d3f8c28db5fcb5d009b099bb
jamilshafayat <jamilshafayat8@gmail.com>
2021-07-20 23:48:42 +0600

    Just random changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import Layout from '../components/Layout/Layout';
-import { Provider } from 'react-redux';
-import { store } from '../store';
+import { Provider } from "react-redux";
+import Layout from "../components/Layout/Layout";
+import { store } from "../store";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
-  )
+	return (
+		<Provider store={store}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -1,70 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
-export default function Article(){
-    const router = useRouter();
-    const { id } = router.query;
-    
-    const [article, setArticle] = useState({isLoading: false, data: []});
+export default function Article() {
+	const router = useRouter();
+	const { id } = router.query;
 
-    useEffect(()=>{
-        setArticle({isLoading: true, data: []});
-        // <Link href={`articles/${index}`}>
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)   
-            .then((res)=>{
-                setArticle({isLoading: false, data: res.data});
-            })
-            .catch((err)=>{
-                setArticle({isLoading: false, data: []});
-            })
-        return ()=>{
-            console.log('Component unmount.');
-        }
-    },[]);
+	const [article, setArticle] = useState({ isLoading: false, data: [] });
 
-    return (
-        <>
-            <div className="all-title-box">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <h2>Article</h2>
-                            <ul className="breadcrumb">
-                                <li className="breadcrumb-item"><a href="#">Article</a></li>
-                                {/* <li className="breadcrumb-item active">Cart</li> */}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="cart-box-main">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="table-main table-responsive">
-                                <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th colSpan="2">Articles</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td className="name-pr">
-                                                <a className="nav-link">{article.data.title}</a>
-                                                <p>
-                                                    {article.data.body}
-                                                </p>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+	useEffect(() => {
+		setArticle({ isLoading: true, data: [] });
+		// <Link href={`articles/${index}`}>
+		axios
+			.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+			.then((res) => {
+				setArticle({ isLoading: false, data: res.data });
+			})
+			.catch((err) => {
+				setArticle({ isLoading: false, data: [] });
+			});
+		return () => {
+			console.log("Component unmount.");
+		};
+	}, []);
+
+	return (
+		<>
+			<div className="all-title-box">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-12">
+							<h2>Article</h2>
+							<ul className="breadcrumb">
+								<li className="breadcrumb-item">
+									<a href="#">Article</a>
+								</li>
+								<li className="breadcrumb-item">
+									<a href="#">Article</a>
+								</li>
+								{/* <li className="breadcrumb-item active">Cart</li> */}
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="cart-box-main">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-12">
+							<div className="table-main table-responsive">
+								<table className="table">
+									<thead>
+										<tr>
+											<th colSpan="2">Articles</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td className="name-pr">
+												<a className="nav-link">{article.data.title}</a>
+												<p>{article.data.body}</p>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
